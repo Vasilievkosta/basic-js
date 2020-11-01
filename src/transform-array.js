c851651b7c7d6272eb68b107d4ec2bc4a6aaa88a
Konstantin <vasilievkosta@gmail.com>
2020-11-01 22:32:30 +0200

    refactor: modified task transform arr

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  //throw new CustomError('Not implemented');
+  // remove line with error and write your code here // throw Error;
+  
+	//if (typeof(arr) !== 'object') return 'Error';
+	if (Array.isArray(arr) === 'false') return 'Error';
+	
+	let outArr = [];
+	let promArr = arr.slice();
+	
+	for (let i = 0; i < promArr.length; i++) {		
+		
+		if (promArr[i] === '--discard-next' && (promArr[i+2] === '--discard-prev' || promArr[i+2] === '--double-prev')) {
+			promArr.splice(i, 3);
+		}	
+	}	
+	for (let i = 0; i < promArr.length; i++) {	
+	
+		if (promArr[i] === '--double-next') {
+			outArr.push(promArr[i + 1]);
+			
+		} else if (promArr[i] === '--double-prev') {		
+			outArr.push(promArr[i - 1]);
+			
+		} else if (promArr[i] === '--discard-prev') {			
+			outArr.splice(i-1, 2);
+			
+		} else {
+			outArr.push(promArr[i]);
+		}	
+	}
+	
+	for (let i = 0; i < outArr.length; i++) {		
+			if (outArr[i] === undefined) {
+			outArr.splice(i, 1);
+			}
+		if (outArr[i] === '--discard-next') {			
+			outArr.splice(i, 2);			
+		}		
+	}	
+	
+	return outArr;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+  
+	if (date === undefined) return 'Unable to determine the time of year!';
+	if (typeof(date) !== 'object') return 'Error';
+	if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('THROWN');
+	
+	let month = date.getMonth();
+	let season = '';
+	
+	
+	switch (month) {
+		case 11:
+		case 0:
+		case 1:		
+			season = 'winter';
+			break;
+		case 2:
+		case 3:
+		case 4:		
+			season = 'spring';
+			break;
+		case 5:
+		case 6:
+		case 7:		
+			season = 'summer';
+			break;
+		case 8:
+		case 9:
+		case 10:		
+			season = 'autumn';
+			break;
+	}	
+	return season;
+}
